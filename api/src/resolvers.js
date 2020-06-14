c11f1c094c6acab0c4d94ad6cea96189db8dc20e
Konstantin Seitz <konstantinseitz@live.de>
2020-06-14 17:30:56 +0200

    Add example resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -1,14 +1,36 @@
 // Resolvers for Schemas. Must match type definitions
 
 module.exports = {
-    Query: {},
-    Mutation: {},
+    Query: {
+        animals(_, { input }, { models }) {
+            return models.Animal.findMany(input || {})
+        },
+        animal(_, { id }, { models }) {
+            return models.animal.findOne({ id })
+        },
+        user(_, __, { models }) {
+            return models.User.findOne()
+        },
+    },
+    Mutation: {
+        addAnimal(_, { input }, { models, user }) {
+            const animal = models.Animal.create({ ...input, user: user.id })
+            return animal
+        },
+    },
     Animal: {
-        img(pet) {
-            return pet.type === 'Goat'
+        owner(animal, _, { models }) {
+            return models.User.findOne({ id: animal.user })
+        },
+        img(animal) {
+            return animal.type === 'Goat'
                 ? 'https://placegoat.net/350/350'
                 : 'http://placekitten.com/350/350'
         },
     },
-    User: {},
+    User: {
+        animal(user, _, { models }) {
+            return models.animal.findMany({ user: user.id })
+        },
+    },
 }
