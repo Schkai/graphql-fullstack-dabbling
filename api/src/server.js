e39ffbba271305e49f2915af36e42d0201fd3def
Konstantin Seitz <konstantinseitz@live.de>
2020-06-14 17:41:19 +0200

    Added Server Params

--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,16 @@
 const { ApolloServer } = require('apollo-server')
 const resolvers = require('./resolvers')
-const types = require('./schema')
+const typeDefs = require('./schema')
 const { models, db } = require('./db')
 
-const server = new ApolloServer()
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context() {
+        const user = db.get('user').value()
+        return { models, db, user }
+    },
+})
 
 server.listen().then(({ url }) => {
     console.log(`Server listening at ${url}`)
